b9dcf2092fe58d5bd2fa1473c18d11f69a65af75
Matt <caijiamx@gmail.com>
2014-09-12 10:24:00 +0800

    rewrite plugin to VideoShortCode class

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
-=== Insert Video With Shortcode ===
+=== Video Short Code ===
 Contributors: caijiamx
-Tags: video, shortcode, youku, ku6, tudou
+Tags: video, short code, youku, ku6, tudou
 Requires at least: 2.8
-Tested up to: 3.5.1
-Stable tag: 1.0
+Tested up to: 4.0
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -11,34 +11,39 @@ This plugin only for some chinese video site.
 
 == Description ==
 
-This plugin can  insert video quikly width shortcode ,specially for some chinese video sites, like  youku.com,tudou.com,ku6.com.
+This plugin can  insert video quikly width short code ,specially for some chinese video sites, like  youku.com,tudou.com,ku6.com.
 
 = Related Links =
 
-* [Documentation](http://www.xbc.me/wordpress-shortcode-for-insert-video/ 
+* [Documentation](https://github.com/caijiamx/video-short-code 
   "Complete usage instructions")
 
 == Installation ==
 
 = Install =
 
-1. Upload the 'insert-video-with-shortcode' folder  to the '/wp-content/plugins/' directory.
+1. Upload the 'video-short-code' folder  to the '/wp-content/plugins/' directory.
 2. Activate the plugin through the 'Plugins' menu in WordPress.
 
 = Uninstall =
 
-1. Deactivate Insert Video With Shortcode in the 'Plugins' menu in Wordpress.
+1. Deactivate Video Short Code in the 'Plugins' menu in Wordpress.
 
 == Frequently Asked Questions ==
 
 = Where can I send bug reports? =
 
-Please visit the Bitbucket repository for [Insert Video With Shortcode](https://bitbucket.org/caijiamx/insert-video-with-shortcode)
-and open an [issue](https://bitbucket.org/caijiamx/insert-video-with-shortcode/issues?status=new&status=open).
+Please visit the Github repository for [Video Short Code](https://github.com/caijiamx/video-short-code)
+and open an [issue](https://github.com/caijiamx/video-short-code/issues).
 
 == Screenshots ==
 
 == Changelog ==
+
+= Version 1.1 - 12 Sep 2014 =
+* Rename plugin to Video Short Code.
+* Update plugin compatible to WordPress 4.0.
+* Rewrite plugin to OOP class.
 
 = Version 1.0 - 5 April 2012 =
 * Initial Release
--- video.php
+++ video.php
@@ -1,62 +1,77 @@
 <?php 
 /**
-Plugin Name: Insert Video With Shortcode
-Plugin URI: http://www.xbc.me/wordpress-shortcode-for-insert-video/
+Plugin Name: Video Short Code
+Plugin URI: https://github.com/caijiamx/video-short-code
 Description: This plugin can  insert video quikly width shortcode ,specially for some chinese video sites, like  youku.com,tudou.com,ku6.com.
 Author: Matt
-Version: 1.0
+Version: 1.1
 Author URI: http://www.xbc.me
 */
-/*function play_youtube($atts){
-	extract(shortcode_atts(array(
-	'code'=>'',
-	'width'=>'480',
-	'height'=>'400'
-	),$atts));
-	$flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://www.youtube.com/v/'.$code.'&hl=en_US&fs=1&autoplay=1"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
-	if(is_single()){
-	return $flash ;
-	}
-	return '';
-}*/
-function play_youku($atts){
-	extract(shortcode_atts(array(
-	'code'=>'',
-	'width'=>'480',
-	'height'=>'400'
-	),$atts));
-	$flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://player.youku.com/player.php/sid/'.$code.'/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
-	if(is_single()){
-	return $flash ;
-	}
-	return '';
+
+class VideoShortCode{
+    public function __construct(){
+        add_shortcode('youku', array($this , 'play_youku'));
+        add_shortcode('tudou', array($this , 'play_tudou'));
+        add_shortcode('ku6', array($this , 'play_ku6'));
+        add_shortcode('youtube', array($this , 'play_youtube'));
+    }
+
+    public function run(){
+
+    }
+
+    public function play_youtube($atts){
+        extract(shortcode_atts(array(
+        'code'=>'',
+        'width'=>'480',
+        'height'=>'400'
+        ),$atts));
+        $flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://www.youtube.com/v/'.$code.'&hl=en_US&fs=1&autoplay=1"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
+        if(is_single()){
+        return $flash ;
+        }
+        return '';
+    }
+
+    public function play_youku($atts){
+        extract(shortcode_atts(array(
+        'code'=>'',
+        'width'=>'480',
+        'height'=>'400'
+        ),$atts));
+        $flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://player.youku.com/player.php/sid/'.$code.'/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
+        if(is_single()){
+        return $flash ;
+        }
+        return '';
+    }
+
+    public function play_tudou($atts){
+        extract(shortcode_atts(array(
+        'code'=>'',
+        'width'=>'480',
+        'height'=>'400'
+        ),$atts));
+        $flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://www.tudou.com/v/'.$code.'/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
+        if(is_single()){
+        return $flash ;
+        }
+        return '';
+    }
+
+    public function play_ku6($atts){
+        extract(shortcode_atts(array(
+        'code'=>'',
+        'width'=>'480',
+        'height'=>'400'
+        ),$atts));
+        $flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://player.ku6.com/refer/'.$code.'/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
+        if(is_single()){
+        return $flash ;
+        }
+        return '';
+    }
 }
-function play_tudou($atts){
-	extract(shortcode_atts(array(
-	'code'=>'',
-	'width'=>'480',
-	'height'=>'400'
-	),$atts));
-	$flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://www.tudou.com/v/'.$code.'/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
-	if(is_single()){
-	return $flash ;
-	}
-	return '';
-}
-function play_ku6($atts){
-	extract(shortcode_atts(array(
-	'code'=>'',
-	'width'=>'480',
-	'height'=>'400'
-	),$atts));
-	$flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://player.ku6.com/refer/'.$code.'/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
-	if(is_single()){
-	return $flash ;
-	}
-	return '';
-}
-	//add_shortcode('youtube', 'play_youtube');
-	add_shortcode('youku', 'play_youku');
-	add_shortcode('tudou', 'play_tudou');
-	add_shortcode('ku6', 'play_ku6');
-?>
+
+$shortcode = new VideoShortCode();
+$shortcode->run();
